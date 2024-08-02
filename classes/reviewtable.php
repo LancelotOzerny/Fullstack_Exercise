7b49e5eaf8ffff324b1784da1b47b56b4242015f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-02 18:15:23 +0300

    Реализовал метод удаления комментариев по id

--- classes/reviewtable.php
+++ classes/reviewtable.php
@@ -3,5 +3,19 @@ require_once dirname($_SERVER['DOCUMENT_ROOT'])  . '/classes/database.php';
 
 class ReviewTable
 {
+    public static function DisplayAll()
+    {
+        $dbArr = DataBase::connection()->query('SELECT * FROM Reviews');
+        $arr = $dbArr->fetchAll(PDO::FETCH_ASSOC);
+        echo '<pre>';
+        print_r($arr);
+        echo '</pre>';
+    }
 
+    public static function delete($id) : bool
+    {
+        $prepare = DataBase::connection()->prepare('DELETE FROM Reviews WHERE id = :id');
+        $prepare->bindValue(':id', $id, PDO::PARAM_INT);
+        return $prepare->execute();
+    }
 }
