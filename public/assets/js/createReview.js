393a6014eb216ac8b9a173ac402b48bbb9c706e7
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-02 21:16:34 +0300

    Реализовал создание комментариев через ajax

--- public/assets/js/createReview.js
+++ public/assets/js/createReview.js
@@ -1,4 +1,22 @@
 function Create()
 {
-
+    $("#CreateButton").addClass('disabled');
+    $.ajax({
+        url: '/crud/create.php',
+        method: 'post',
+        data: {
+            'email' : $('#InputEmail').val(),
+            'name' : $('#InputName').val(),
+            'text' : $('#InputMessage').val(),
+        },
+        success: (data) => {
+            let dataList = JSON.parse(data);
+            console.log(dataList);
+            RewriteTable();
+            $("#CreateButton").removeClass('disabled');
+        },
+        fail: (data) => {
+            $("#CreateButton").removeClass('disabled');
+        }
+    });
 }
--- public/crud/create.php
+++ public/crud/create.php
@@ -1,4 +1,40 @@
 <?php
 require_once dirname($_SERVER['DOCUMENT_ROOT'])  . '/classes/reviewtable.php';
+require_once dirname($_SERVER['DOCUMENT_ROOT'])  . '/classes/validator.php';
 
-die('[create]');
+$data = [];
+
+if (empty($_POST))
+{
+    $data['errors'][] = 'Отсутствуют входные данные!';
+}
+else
+{
+    $prepareData = [
+        'email' => trim($_POST['email'] ?? ''),
+        'name' => trim($_POST['name'] ?? ''),
+        'text' => trim($_POST['text'] ?? ''),
+    ];
+
+    $validator = new Validator($prepareData);
+
+    $data['errors']['email'] = join('<br/>', $validator->getEmailErrors());
+    $data['errors']['name'] = join('<br/>', $validator->getNameErrors());
+    $data['errors']['text'] = join('<br/>', $validator->getTextErrors());
+
+    foreach ($data['errors'] as $errorGroup)
+    {
+        if (empty($errorGroup) === false)
+        {
+            die(json_encode($data));
+        }
+    }
+
+    $prepareData['email'] = htmlspecialchars($prepareData['email']);
+    $prepareData['name'] = htmlspecialchars($prepareData['name']);
+    $prepareData['text'] = htmlspecialchars($prepareData['text']);
+
+    $data['result'] = ReviewTable::Create($prepareData);
+}
+
+die(json_encode($data));
