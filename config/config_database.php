e99ac929cc33d27fe26df9b90b1f7bd6d73cf0b4
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-02 18:01:40 +0300

    Убрал настройки БД из отслеживаемых

--- config/config_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config_Database
-{
-    public static string $host = 'localhost';
-    public static string $dbname = '';
-    public static string $username = 'root';
-    public static string $password = '';
-}
