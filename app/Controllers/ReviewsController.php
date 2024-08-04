8fc0833f03559386384ce4cd2def06a7e55e76d6
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-04 17:28:04 +0300

    написал чтение всех записей и удаление по id

--- app/Controllers/ReviewsController.php
+++ app/Controllers/ReviewsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\ReviewsModel;
+
 class ReviewsController extends BaseController
 {
     public function list()
@@ -11,16 +13,74 @@ class ReviewsController extends BaseController
 
     public function create()
     {
-        return json_encode([]);
+        $data = [
+            'errors' => []
+        ];
+
+        if (empty($_POST))
+        {
+            $data['errors'][] = 'Отсутствуют входные данные!';
+        }
+        else
+        {
+            $prepareData = [
+                'email' => trim($_POST['email'] ?? ''),
+                'name' => trim($_POST['name'] ?? ''),
+                'text' => trim($_POST['text'] ?? ''),
+            ];
+
+            //$validator = new Validator($prepareData);
+
+            //$data['errors']['email'] = join('<br/>', $validator->getEmailErrors());
+            //$data['errors']['name'] = join('<br/>', $validator->getNameErrors());
+            //$data['errors']['text'] = join('<br/>', $validator->getTextErrors());
+
+            foreach ($data['errors'] as $errorGroup)
+            {
+                if (empty($errorGroup) === false)
+                {
+                    die(json_encode($data));
+                }
+            }
+
+            $prepareData['email'] = htmlspecialchars($prepareData['email']);
+            $prepareData['name'] = htmlspecialchars($prepareData['name']);
+            $prepareData['text'] = htmlspecialchars($prepareData['text']);
+
+            //$data['result'] = ReviewTable::Create($prepareData);
+
+            return $data;
+        }
     }
 
     public function read()
     {
-        return json_encode([]);
+        $model = new ReviewsModel();
+        $data['reviews'] = $model->findAll();
+        return json_encode($data);
     }
 
     public function delete()
     {
-        return json_encode([]);
+        $response = [];
+        $id = $this->request->getPost('id');
+        if ($id)
+        {
+            $model = new ReviewsModel();
+            if ($model->delete($id))
+            {
+                $response['result'] = true;
+            }
+            else
+            {
+                $response['errors'][]  = 'Ошибка при удалении записи';
+            }
+        }
+        else
+        {
+            $response['errors'][]  = 'Не передан ID записи для удаления';
+        }
+            return json_encode($response);
     }
+
 }
