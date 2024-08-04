fc771dceae2330d7271c4608a684d19bdd93689b
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-04 17:58:37 +0300

    Реализовал создание комментариев с валидацией

--- app/Controllers/ReviewsController.php
+++ app/Controllers/ReviewsController.php
@@ -13,45 +13,20 @@ class ReviewsController extends BaseController
 
     public function create()
     {
+        $model = new ReviewsModel();
+        $response = [];
+
         $data = [
-            'errors' => []
+            'email' => $this->request->getPost('email'),
+            'name' => $this->request->getPost('name'),
+            'text' => $this->request->getPost('text'),
         ];
 
-        if (empty($_POST))
-        {
-            $data['errors'][] = 'Отсутствуют входные данные!';
-        }
-        else
-        {
-            $prepareData = [
-                'email' => trim($_POST['email'] ?? ''),
-                'name' => trim($_POST['name'] ?? ''),
-                'text' => trim($_POST['text'] ?? ''),
-            ];
+        $response = $model->createReview($data);
 
-            //$validator = new Validator($prepareData);
-
-            //$data['errors']['email'] = join('<br/>', $validator->getEmailErrors());
-            //$data['errors']['name'] = join('<br/>', $validator->getNameErrors());
-            //$data['errors']['text'] = join('<br/>', $validator->getTextErrors());
-
-            foreach ($data['errors'] as $errorGroup)
-            {
-                if (empty($errorGroup) === false)
-                {
-                    die(json_encode($data));
-                }
-            }
-
-            $prepareData['email'] = htmlspecialchars($prepareData['email']);
-            $prepareData['name'] = htmlspecialchars($prepareData['name']);
-            $prepareData['text'] = htmlspecialchars($prepareData['text']);
-
-            //$data['result'] = ReviewTable::Create($prepareData);
-
-            return $data;
-        }
+        return json_encode($response);
     }
+
 
     public function read()
     {
--- app/Models/ReviewsModel.php
+++ app/Models/ReviewsModel.php
@@ -4,4 +4,57 @@ namespace App\Models;
 class ReviewsModel extends \CodeIgniter\Model
 {
     protected $table = 'reviews';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['email', 'name', 'text'];
+
+
+    public function createReview($data)
+    {
+        if ($this->validateReview($data))
+        {
+            if ($this->insert($data))
+            {
+                return ['result' => true];
+            }
+            else
+            {
+                return ['errors'=> ['Ошибка при создании пользователя']];
+            }
+        }
+        else
+        {
+            return ['errors' => $this->validator->getErrors()];
+        }
+    }
+    protected function validateReview($data)
+    {
+        $this->validator = \Config\Services::validation();
+
+        $rules = [
+            'email' => [
+                'rules' => 'required|valid_email',
+                'errors' => [
+                    'required' => 'Поле Email обязательно!',
+                    'valid_email' => 'Поле Email некорректно!'
+                ]
+            ],
+            'name' => [
+                'rules' => 'required|alpha_numeric_space|min_length[4]|max_length[16]',
+                'errors' => [
+                    'required' => 'Поле Имя обязательно',
+                    'alpha_numeric_space' => 'Имя может содержать только буквы, цифры и пробелы',
+                    'min_length' => 'Имя может содержать от 4 до 16 символов',
+                    'max_length' => 'Имя может содержать от 4 до 16 символов'
+                ]
+            ],
+            'text' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Поле сообщение обязательно к заполнению'
+                ]
+            ]
+        ];
+
+        return $this->validator->setRules($rules)->run($data);
+    }
 }
