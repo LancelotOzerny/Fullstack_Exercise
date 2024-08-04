e70777dbc346c5bc8fb6861a78a6f109cc912962
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-04 17:16:43 +0300

    Перевел CRUD маршруты на post режим

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'ReviewsController::list');
-$routes->get('reviews/create/', 'ReviewsController::create');
-$routes->get('reviews/read/', 'ReviewsController::read');
-$routes->get('reviews/delete/', 'ReviewsController::delete');
+$routes->post('reviews/create/', 'ReviewsController::create');
+$routes->post('reviews/read/', 'ReviewsController::read');
+$routes->post('reviews/delete/', 'ReviewsController::delete');
